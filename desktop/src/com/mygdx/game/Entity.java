f1837099b63784f4a4954465a3aa71d553ae5733
tanwm98 <2301777@sit.singaporetech.edu.sg>
2024-02-10 16:14:09 +0800

    copied from lab to project first as reference

--- desktop/src/com/mygdx/game/Entity.java
+++ desktop/src/com/mygdx/game/Entity.java
@@ -1,4 +1,70 @@
 package com.mygdx.game;
 
-public class Entity {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.Texture;
+import java.util.Arrays;
+
+
+public abstract class Entity implements iMoveable {
+    public abstract void move();
+    public abstract void draw();
+    public abstract void update();
+
+
+    private Color color;
+    private float x,y,speed;
+    private float[] position;
+    private Color[] colors;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+    public float[] getPosition() {
+        return Arrays.copyOf(position, position.length);
+    }
+
+    public void setPosition(float[] position) {
+        this.position = Arrays.copyOf(position, position.length);
+    }
+
+    public Color[] getColors() {
+        return colors;
+    }
+
+    public void setColors(Color[] colors) {
+        this.colors = colors;
+    }
+
 }
--- desktop/src/com/mygdx/game/EntityManager.java
+++ desktop/src/com/mygdx/game/EntityManager.java
@@ -1,4 +1,34 @@
 package com.mygdx.game;
 
-public class EntityManager {
+import java.util.*;
+import com.badlogic.gdx.graphics.Color;
+
+public class EntityManager{
+    private List<Entity> entityList;
+
+    public EntityManager()
+    {
+        entityList = new ArrayList<>();
+        entityList.add(new TextureObject("droplet.png", new float[]{200.0f,200.0f}, 2, true));
+        entityList.add(new TextureObject("bucket.png", new float[]{200.0f,0.0f},false));
+        entityList.add(new Circle(new float[]{500.0f,200.0f},50, Color.RED));
+        entityList.add(new Triangle(new float[]{0.0f,10.0f,170.0f,10.0f,100.0f,150.0f},
+                new Color[]{Color.GREEN,Color.GREEN,Color.GREEN},10));
+    }
+    public void drawEntities()
+    {
+        for(Entity entity : entityList) //for each entity in the list
+        {
+            entity.draw(); //draw the entity
+        }
+    }
+    public void moveEntities()
+    {
+        for(Entity entity : entityList)
+        {
+            entity.move();
+        }
+    }
+
+
 }
