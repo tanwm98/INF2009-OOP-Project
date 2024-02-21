addc7ca3ad90b0f6aba7be137242510aaf5fc57b
Tan Zhi Xin <144520526+Zhixin34@users.noreply.github.com>
2024-02-21 18:34:48 +0800

    Update InputManager.java

--- desktop/src/com/mygdx/game/InputManager.java
+++ desktop/src/com/mygdx/game/InputManager.java
@@ -1,12 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
-import com.mygdx.game.Entity.Entity;
-import com.mygdx.game.Entity.Paddle;
-import com.mygdx.game.Interfaces.IMovement;
 
 public class InputManager {
 	//KeyPressed
