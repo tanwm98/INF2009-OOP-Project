457a6b9b84afd5e46f2f9c08b4f901a044422f0c
carine <119272670+cring25@users.noreply.github.com>
2024-03-15 17:43:17 +0800

    created Asteroid entity

--- desktop/src/com/mygdx/game/Entity/Asteroid.java
+++ desktop/src/com/mygdx/game/Entity/Asteroid.java
@@ -1,15 +1,43 @@
 package com.mygdx.game.Entity;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Asteroid extends Entity {
-    public void update() {
+	private Texture tex;
+	
+	SpriteBatch batch = new SpriteBatch();
+	
+    public Asteroid() {
+
+    }
+    
+    public Asteroid(String filePath) {
+    	super();
+		tex = new Texture(Gdx.files.internal(filePath));
+    }
+    
+	public Texture getTexture() {
+		return tex;
+	}
+	
+	public void setTexture(Texture t) {
+		tex = t;
+	}
+	
+	public void update() {
+        System.out.printf("Asteroid pos X:%f and Y: %f\n",super.getX(),super.getY());
     }
 
     public void render() {
-
+    	batch.begin();
+    	getoutputManager().draw(batch, getTexture(), super.getX(), super.getY());
+		batch.end();
     }
 
     public void dispose() {
-
+    	batch.dispose();
     }
 
     public void move() {
