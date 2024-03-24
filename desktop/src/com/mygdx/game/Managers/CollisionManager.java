05c6163b7e729c0b447c70843758f82f5df9d996
Dogephin <64132341+Dogephin@users.noreply.github.com>
2024-03-24 14:57:16 +0800

    Fixed Planet Collision

--- desktop/src/com/mygdx/game/Managers/CollisionManager.java
+++ desktop/src/com/mygdx/game/Managers/CollisionManager.java
@@ -8,12 +8,17 @@ import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.game.Entity.Asteroid;
 import com.mygdx.game.Entity.Entity;
 import com.mygdx.game.Entity.Planet;
+import com.mygdx.game.Entity.Satellite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Circle;
 
 
 public class CollisionManager{
 
 	private Rectangle object1,object3;
+	private float planetx , planety , planetrad;
 	private Circle object2;
 	private boolean collide;
     private List<Entity> CollideableObjects;
@@ -37,14 +42,19 @@ public class CollisionManager{
 		collide = false;
 		object1 = new Rectangle(e1.getX() , e1.getY() , e1.getWidth() , e1.getHeight());
 		if(e2 instanceof Asteroid || e2 instanceof Satellite) {
-			//More accurate collision for asteroid and satellite instead of using circle
+			//More accurate collision for asteroid instead of using circle
 			object3 = new Rectangle(e2.getX() , e2.getY() , e2.getWidth() , e2.getHeight());
 			if(Intersector.overlaps(object1 , object3) == true) {
 				collide = true;
 			}
 		}
 		else {
-			object2 = new Circle(e2.getX() , e2.getY() , e2.getHeight());
+			planetx = e2.getX()+e2.getWidth()/2;
+			planety = e2.getY()+e2.getHeight()/2;
+			planetrad = e2.getWidth()/2;
+			
+			object2 = new Circle(planetx , planety, planetrad);
+	
 			if(Intersector.overlaps(object2 , object1) == true) {
 				collide = true;
 			}
