4c6a83110d950918fbd186772128fa598b3a054d
Dogephin <64132341+Dogephin@users.noreply.github.com>
2024-03-25 15:22:16 +0800

    Change ICollideable

--- desktop/src/com/mygdx/game/Interfaces/ICollideable.java
+++ desktop/src/com/mygdx/game/Interfaces/ICollideable.java
@@ -2,4 +2,8 @@ package com.mygdx.game.Interfaces;
 
 public interface ICollideable {
     public void collide(boolean collide);
+    public float getX();
+    public float getY();
+    public float getWidth();
+    public float getHeight();
 }
