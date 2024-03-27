066e1e98cc030e3fec50f100a73def2eeba749ad
Dogephin <64132341+Dogephin@users.noreply.github.com>
2024-03-27 22:40:04 +0800

    Changed hitbox for satellite

--- desktop/src/com/mygdx/game/Managers/CollisionManager.java
+++ desktop/src/com/mygdx/game/Managers/CollisionManager.java
@@ -19,7 +19,7 @@ import com.badlogic.gdx.math.Circle;
 public class CollisionManager{
 
 	private Rectangle object1,object3;
-	private float planetx , planety , planetrad;
+	private float objectx , objecty , planetrad;
 	private Circle object2;
 	private boolean collide;
     private List<ICollideable> CollideableObjects;
@@ -29,9 +29,9 @@ public class CollisionManager{
 	}
 	
     public void addEntity(Entity entity) {
-        if(entity.isCollideable()) // If the entity is AI controlled, add it to the list
+        if(entity.isCollideable()) 
         {
-                CollideableObjects.add(entity);
+        	CollideableObjects.add(entity);
         }
     }
     
@@ -42,19 +42,31 @@ public class CollisionManager{
 	public boolean detectCollision(ICollideable e1 , ICollideable e2) {
 		collide = false;
 		object1 = new Rectangle(e1.getX() , e1.getY() , e1.getWidth() , e1.getHeight());
-		if(e2 instanceof Asteroid || e2 instanceof Satellite) {
+		
+		//Applicable for non spinning moving objects
+		if(e2 instanceof Asteroid) {
 			//More accurate collision for asteroid instead of using circle
 			object3 = new Rectangle(e2.getX() , e2.getY() , e2.getWidth() , e2.getHeight());
 			if(Intersector.overlaps(object1 , object3) == true) {
 				collide = true;
 			}
 		}
+		//Applicable to All spinnable non circular objects
+		else if(e2 instanceof Satellite){
+		    objectx = e2.getX()+e2.getWidth()/4;
+		    objecty = e2.getY()+e2.getHeight()/4;
+			object3 = new Rectangle(e2.getX() , e2.getY() , e2.getWidth() , e2.getHeight());
+			if(Intersector.overlaps(object1 , object3) == true) {
+				collide = true;
+			}
+		}
+		//Use for circular spinning objects
 		else {
-			planetx = e2.getX()+e2.getWidth()/2;
-			planety = e2.getY()+e2.getHeight()/2;
+			objectx = e2.getX()+e2.getWidth()/2;
+			objecty = e2.getY()+e2.getHeight()/2;
 			planetrad = e2.getWidth()/2;
 			
-			object2 = new Circle(planetx , planety, planetrad);
+			object2 = new Circle(objectx , objecty, planetrad);
 	
 			if(Intersector.overlaps(object2 , object1) == true) {
 				collide = true;
